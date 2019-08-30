e10802b14ee8ba4fc22cffeb9cb9eed8df8ad4f1
Guido Bitonte <bitonteguido@gmail.com>
2019-08-30 09:47:30 -0300

    Hello World

---
+++ Day 0 Hello, World..cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution {
+    static void Main(String[] args) {
+        String inputString; 
+        inputString = Console.ReadLine(); 
+        Console.WriteLine("Hello, World.");
+    }
+}
