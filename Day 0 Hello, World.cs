eb82f4defe435422ad604bffd037d24581dae0ec
Guido Bitonte <bitonteguido@gmail.com>
2019-08-30 09:49:40 -0300

    C#

--- Day 0 Hello, World.cs
+++ Day 0 Hello, World.cs
@@ -4,8 +4,10 @@ using System.IO;
 
 class Solution {
     static void Main(String[] args) {
+
         String inputString; 
         inputString = Console.ReadLine(); 
         Console.WriteLine("Hello, World.");
+
     }
 }
